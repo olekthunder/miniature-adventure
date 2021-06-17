dd139a6e132d947efb8a5bbca6cab5be2ed1784f
Oleksandr Zinkevych <zso040399@gmail.com>
2021-06-17 16:31:33 +0300

    Task 5 done

---
+++ consumer5/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+var Q_NAME = "tq5" 
+
+func rmqConnect() *amqp.Connection {
+	for {
+		conn, err := amqp.Dial("amqp://guest:guest@rmq:5672/")
+		if err == nil {
+			fmt.Println("Connected to rmq")
+			return conn
+		} else {
+			fmt.Println("Failed to connect, retrying...")
+			time.Sleep(time.Second * 2)
+		}
+	}
+}
+
+func main() {
+	conn := rmqConnect()
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer ch.Close()
+	_, err = ch.QueueDeclare(Q_NAME, false, false, false, false, amqp.Table{"x-message-ttl": 5000})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Sleeping for 10 seconds...")
+	time.Sleep(time.Second * 10)
+	for {
+		msgs, err := ch.Consume(
+			Q_NAME,
+			"",
+			true,
+			false,
+			false,
+			false,
+			nil,
+		)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		for m := range msgs {
+			fmt.Printf("Consumed %v\n", string(m.Body))
+		}
+	}
+}
---
+++ producer5/go.mod
@@ -0,0 +1,5 @@
+module github.com/olekthunder/miniature-adventure/producer5
+
+go 1.16
+
+require github.com/streadway/amqp v1.0.0
